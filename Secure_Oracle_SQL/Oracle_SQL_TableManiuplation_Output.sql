a2d4e1f72925ad25fa7fef0dd42e321f39ba59dc
Hiren Shah <38018755+Hshah1989@users.noreply.github.com>
2021-08-05 09:05:51 -0400

    Add files via upload

---
+++ Secure_Oracle_SQL/Oracle_SQL_TableManiuplation_Output.sql
@@ -0,0 +1,252 @@
+
+Table ENGINEERS dropped.
+
+
+Table FACULTY dropped.
+
+
+Table CLASSES dropped.
+
+
+Table CLASSENROLLMENTS dropped.
+
+
+Table ENGINEERS created.
+
+
+Table FACULTY created.
+
+
+Table CLASSES created.
+
+
+Table CLASSENROLLMENTS created.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+1 row inserted.
+
+
+       EID LASTNAME                                 FIRSTNAME                                EMAIL                                    GRADDATE 
+---------- ---------------------------------------- ---------------------------------------- ---------------------------------------- ---------
+        15 Tabitha                                  Croker                                   TabithaCroker@gmail.com                  09-JAN-21
+        14 Chanda                                   Lamphear                                 ChandaLamphear@gmail.com                 09-JAN-21
+        13 Opal                                     Harrel                                   OpalHarrel@gmail.com                     09-JAN-21
+        12 Chad                                     Roehr                                    ChadRoehr@gmail.com                      09-JAN-21
+        11 Carman                                   Gardener                                 CarmanGardener@gmail.com                 09-JAN-21
+        10 Nereida                                  Victorino                                NereidaVictorino@gmail.com               09-JAN-21
+         9 Shanti                                   Isaac                                    ShantiIsaac@gmail.com                    09-JAN-21
+         8 Elfriede                                 Loder                                    ElfriedeLoder@gmail.com                  09-JAN-21
+         7 Lindsay                                  Lard                                     LindsayLard@gmail.com                    09-JAN-21
+         6 Loretta                                  Suess                                    LorettaSuess@gmail.com                   09-JAN-21
+         5 Deidre                                   Sager                                    DeidreSager@gmail.com                    09-JAN-21
+
+       EID LASTNAME                                 FIRSTNAME                                EMAIL                                    GRADDATE 
+---------- ---------------------------------------- ---------------------------------------- ---------------------------------------- ---------
+         4 Sage                                     Heffelfinger                             SageHeffelfinger@gmail.com               09-JAN-21
+         3 Gregory                                  Paez                                     GregoryPaez@gmail.com                    09-JAN-21
+         2 Roxann                                   Engh                                     RoxannEngh@gmail.com                     09-JAN-21
+         1 Morton                                   Harshbarger                              MortonHarshbarger@gmail.com              09-JAN-21
+
+15 rows selected. 
+
+
+       FID LASTNAME                                 FIRSTNAME                                EMAIL                                    HIREDATE 
+---------- ---------------------------------------- ---------------------------------------- ---------------------------------------- ---------
+         3 Leesa                                    Bertolini                                LeesaBertolini@gmail.com                 23-JUL-20
+         2 Eleanor                                  Ackerley                                 EleanorAckerley@gmail.com                22-JAN-21
+         1 Coralie                                  Mirelez                                  CoralieMirelez@gmail.com                 21-APR-19
+
+
+       CID SUBJECT                                  CATALOGNBR                               TITLE                                   
+---------- ---------------------------------------- ---------------------------------------- ----------------------------------------
+         3 Biology                                  7 86416 2                                Bio350                                  
+         2 Mathamatics                              7 47021 3                                Calc3                                   
+         1 Engineering                              7 46015 2                                Engin101                                
+
+
+      ENID        CID        FID        EID
+---------- ---------- ---------- ----------
+        15          1          3         15
+        14          3          1         14
+        13          1          3         13
+        12          3          1         12
+        11          2          1         11
+        10          2          2         10
+         9          3          1          9
+         8          2          1          8
+         7          3          2          7
+         6          2          3          6
+         5          3          2          5
+
+      ENID        CID        FID        EID
+---------- ---------- ---------- ----------
+         4          2          1          4
+         3          2          1          3
+         2          3          3          2
+         1          1          1          1
+
+15 rows selected. 
+
+
+1 row updated.
+
+
+1 row updated.
+
+
+1 row updated.
+
+
+1 row deleted.
+
+
+LASTNAME                                 FIRSTNAME                                LASTNAME                                 EMAIL                                    SUBJECT                                  TITLE                                   
+---------------------------------------- ---------------------------------------- ---------------------------------------- ---------------------------------------- ---------------------------------------- ----------------------------------------
+Roxann                                   Engh                                     Leesa                                    LeesaBertolini@gmail.com                 IOT Cyber                                Bio350                                  
+Gregory                                  Paez                                     Friendship                               CoralieMirelez@gmail.com                 Mathamatics                              Calc3                                   
+Sage                                     Heffelfinger                             Friendship                               CoralieMirelez@gmail.com                 Mathamatics                              Calc3                                   
+Deidre                                   Sager                                    Eleanor                                  EleanorAckerley@gmail.com                IOT Cyber                                Bio350                                  
+Loretta                                  Suess                                    Leesa                                    LeesaBertolini@gmail.com                 Mathamatics                              Calc3                                   
+Lindsay                                  Lard                                     Eleanor                                  EleanorAckerley@gmail.com                IOT Cyber                                Bio350                                  
+Elfriede                                 Loder                                    Friendship                               CoralieMirelez@gmail.com                 Mathamatics                              Calc3                                   
+Shanti                                   Isaac                                    Friendship                               CoralieMirelez@gmail.com                 IOT Cyber                                Bio350                                  
+Nereida                                  Victorino                                Eleanor                                  EleanorAckerley@gmail.com                Mathamatics                              Calc3                                   
+Carman                                   Gardener                                 Friendship                               CoralieMirelez@gmail.com                 Mathamatics                              Calc3                                   
+Chad                                     Roehr                                    Friendship                               CoralieMirelez@gmail.com                 IOT Cyber                                Bio350                                  
+
+LASTNAME                                 FIRSTNAME                                LASTNAME                                 EMAIL                                    SUBJECT                                  TITLE                                   
+---------------------------------------- ---------------------------------------- ---------------------------------------- ---------------------------------------- ---------------------------------------- ----------------------------------------
+Opal                                     Harrel                                   Leesa                                    LeesaBertolini@gmail.com                 Engineering                              Engin101                                
+Chanda                                   Lamphear                                 Friendship                               CoralieMirelez@gmail.com                 IOT Cyber                                Bio350                                  
+Tabitha                                  Croker                                   Leesa                                    LeesaBertolini@gmail.com                 Engineering                              Engin101                                
+
+14 rows selected. 
+
+
+ENROLLMENTID ENG_LAST                                 ENG_FIRST                                F_LAST                                   F_EMAIL                                  CLASS_SUBJ                               CLASS_TITLE                             
+------------ ---------------------------------------- ---------------------------------------- ---------------------------------------- ---------------------------------------- ---------------------------------------- ----------------------------------------
+           2 Roxann                                   Engh                                     Leesa                                    LeesaBertolini@gmail.com                 IOT Cyber                                Bio350                                  
+           3 Gregory                                  Paez                                     Friendship                               CoralieMirelez@gmail.com                 Mathamatics                              Calc3                                   
+           4 Sage                                     Heffelfinger                             Friendship                               CoralieMirelez@gmail.com                 Mathamatics                              Calc3                                   
+           5 Deidre                                   Sager                                    Eleanor                                  EleanorAckerley@gmail.com                IOT Cyber                                Bio350                                  
+           6 Loretta                                  Suess                                    Leesa                                    LeesaBertolini@gmail.com                 Mathamatics                              Calc3                                   
+           7 Lindsay                                  Lard                                     Eleanor                                  EleanorAckerley@gmail.com                IOT Cyber                                Bio350                                  
+           8 Elfriede                                 Loder                                    Friendship                               CoralieMirelez@gmail.com                 Mathamatics                              Calc3                                   
+           9 Shanti                                   Isaac                                    Friendship                               CoralieMirelez@gmail.com                 IOT Cyber                                Bio350                                  
+          10 Nereida                                  Victorino                                Eleanor                                  EleanorAckerley@gmail.com                Mathamatics                              Calc3                                   
+          11 Carman                                   Gardener                                 Friendship                               CoralieMirelez@gmail.com                 Mathamatics                              Calc3                                   
+          12 Chad                                     Roehr                                    Friendship                               CoralieMirelez@gmail.com                 IOT Cyber                                Bio350                                  
+
+ENROLLMENTID ENG_LAST                                 ENG_FIRST                                F_LAST                                   F_EMAIL                                  CLASS_SUBJ                               CLASS_TITLE                             
+------------ ---------------------------------------- ---------------------------------------- ---------------------------------------- ---------------------------------------- ---------------------------------------- ----------------------------------------
+          13 Opal                                     Harrel                                   Leesa                                    LeesaBertolini@gmail.com                 Engineering                              Engin101                                
+          14 Chanda                                   Lamphear                                 Friendship                               CoralieMirelez@gmail.com                 IOT Cyber                                Bio350                                  
+          15 Tabitha                                  Croker                                   Leesa                                    LeesaBertolini@gmail.com                 Engineering                              Engin101                                
+
+14 rows selected. 
+
